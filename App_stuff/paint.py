24e59933f048486b99372a669fdd9a2ffc847a08
patrickshortall26 <gz19139@bristol.ac.uk>
2022-05-05 21:11:12 +0100

    add examples of paint apps

---
+++ App_stuff/paint.py
@@ -0,0 +1,17 @@
+from tkinter import *
+root = Tk()
+root.title("Paint Application")
+root.geometry("500x350")
+def paint(event):
+    # get x1, y1, x2, y2 co-ordinates
+    x1, y1 = (event.x-1), (event.y-1)
+    x2, y2 = (event.x+1), (event.y+1)
+    color = "black"
+    # display the mouse movement inside canvas
+    wn.create_oval(x1, y1, x2, y2, fill=color, outline=color)
+# create canvas
+wn=Canvas(root, width=500, height=350, bg='white')
+# bind mouse event with canvas(wn)
+wn.bind('<B1-Motion>', paint)
+wn.pack()
+root.mainloop()
